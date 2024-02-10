0d0c73cd45164186ea3777697afee4833725c8fe
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-02-10 14:12:23 +0500

    Gemini animation Added

--- src/app/components/ui/google-gemini-effect.tsx
+++ src/app/components/ui/google-gemini-effect.tsx
@@ -22,7 +22,7 @@ export const GoogleGeminiEffect = ({
   return (
     <div className={cn("sticky top-10", className)}>
       <p className="text-lg md:text-7xl font-normal pb-4 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
-        {title || `Build with Aceternity UI`}
+        {title || `Tailwind Css and Framer Motion`}
       </p>
       <p className="text-xs md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
         {description ||
@@ -31,7 +31,7 @@ export const GoogleGeminiEffect = ({
       </p>
       <div className="w-full h-[890px] -top-60 md:-top-40  flex items-center justify-center bg-red-transparent absolute ">
         <button className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto ">
-          ui.aceternity.com
+          Gemini.com
         </button>
       </div>
       <svg
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useScroll, useTransform } from "framer-motion";
 import React from "react";
 import { GoogleGeminiEffect } from "./components/ui/google-gemini-effect";
+import { TypewriterEffectSmooth } from "./components/ui/typewriter-effect";
 
 export default function GoogleGeminiEffectDemo() {
   const ref = React.useRef(null);
@@ -16,6 +17,24 @@ export default function GoogleGeminiEffectDemo() {
   const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
   const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
 
+  const words = [
+    {
+      text: "Build",
+    },
+    {
+      text: "awesome",
+    },
+    {
+      text: "apps",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "TailwindCSS & Framer Motion",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
     <>
       <div
@@ -32,7 +51,20 @@ export default function GoogleGeminiEffectDemo() {
           ]}
         />
       </div>
-      <h1 className="h-[100vh]">Hello</h1>
+      <div className="flex flex-col items-center justify-center h-[40rem]  ">
+        <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+          The road to freedom starts from here
+        </p>
+        <TypewriterEffectSmooth words={words} />
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+          <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+            Join now
+          </button>
+          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+            Signup
+          </button>
+        </div>
+      </div>
     </>
   );
 }
